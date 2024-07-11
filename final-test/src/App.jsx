3a554ec03d0f2a1f45f2d82eabe45c773d64c852
kiennguyenduc123 <gunski01@gmail.com>
2024-07-11 22:17:20 +0700

    init project

---
+++ final-test/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './component/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+     <div className='Web'>
+        <h1 className='title'>#todo</h1>
+        <div><Navbar/></div>
+        
+     </div>
+    </>
+     
+  )
+}
+
+export default App
